aa6a6a365e2c79c15fdf9904d440092caafa3df4
Andy Wong <awong@cogolabs.com>
2014-11-24 10:51:28 -0500

    Updated Admin panel columns

--- rango/admin.py
+++ rango/admin.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from rango.models import Category, Page
 
 class PageAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'url')
+    list_display = ('title', 'category', 'url', 'views')
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'views', 'likes')
